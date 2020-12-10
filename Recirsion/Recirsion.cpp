d50c5b0dd1b8d70b8d948978e76ecb1f5d46aeb4
TimCheers <73362594+TimCheers@users.noreply.github.com>
2020-12-10 23:30:36 +0500

    Улучшил вывод

--- Recirsion/Recirsion.cpp
+++ Recirsion/Recirsion.cpp
@@ -2,367 +2,121 @@
 
 using namespace std;
 
-void foo(int num, int numS)
+void Out(int res)
 {
-    int res;
+    switch (res)
+    {
+    case(10):
+        cout << 'A';
+        break;
+    case(11):
+        cout << 'B';
+        break;
+    case(12):
+        cout << 'C';
+        break;
+    case(13):
+        cout << 'D';
+        break;
+    case(14):
+        cout << 'E';
+        break;
+    case(15):
+        cout << 'F';
+        break;
+    case(16):
+        cout << 'G';
+        break;
+    case(17):
+        cout << 'H';
+        break;
+    case(18):
+        cout << 'I';
+        break;
+    case(19):
+        cout << 'J';
+        break;
+    case(20):
+        cout << 'K';
+        break;
+    case(21):
+        cout << 'L';
+        break;
+    case(22):
+        cout << 'M';
+        break;
+    case(23):
+        cout << 'N';
+        break;
+    case(24):
+        cout << 'O';
+        break;
+    case(25):
+        cout << 'P';
+        break;
+    case(26):
+        cout << 'Q';
+        break;
+    case(27):
+        cout << 'R';
+        break;
+    case(28):
+        cout << 'S';
+        break;
+    case(29):
+        cout << 'T';
+        break;
+    case(30):
+        cout << 'U';
+        break;
+    case(31):
+        cout << 'V';
+        break;
+    case(32):
+        cout << 'W';
+        break;
+    case(33):
+        cout << 'X';
+        break;
+    case(34):
+        cout << 'Y';
+        break;
+    case(35):
+        cout << 'Z';
+        break;
+    default:
+        cout << res;
+        break;
+    }
+}
+void foo(long int num, int numS)
+{
+    long int res;
     res = num % numS;
     num /= numS;
     if (num>=numS)
     {
         foo(num, numS);
-        switch (res)
-        {
-        case(10):
-            cout << 'A';
-            break;
-        case(11):
-            cout << 'B';
-            break;
-        case(12):
-            cout << 'C';
-            break;
-        case(13):
-            cout << 'D';
-            break;
-        case(14):
-            cout << 'E';
-            break;
-        case(15):
-            cout << 'F';
-            break;
-        case(16):
-            cout << 'G';
-            break;
-        case(17):
-            cout << 'H';
-            break;
-        case(18):
-            cout << 'I';
-            break;
-        case(19):
-            cout << 'J';
-            break;
-        case(20):
-            cout << 'K';
-            break;
-        case(21):
-            cout << 'L';
-            break;
-        case(22):
-            cout << 'M';
-            break;
-        case(23):
-            cout << 'N';
-            break;
-        case(24):
-            cout << 'O';
-            break;
-        case(25):
-            cout << 'P';
-            break;
-        case(26):
-            cout << 'Q';
-            break;
-        case(27):
-            cout << 'R';
-            break;
-        case(28):
-            cout << 'S';
-            break;
-        case(29):
-            cout << 'T';
-            break;
-        case(30):
-            cout << 'U';
-            break;
-        case(31):
-            cout << 'V';
-            break;
-        case(32):
-            cout << 'W';
-            break;
-        case(33):
-            cout << 'X';
-            break;
-        case(34):
-            cout << 'Y';
-            break;
-        case(35):
-            cout << 'Z';
-            break;
-        default:
-            cout << res;
-            break;
-        }
+        Out(res);
     }
     else
     {
         if (num == 0)
         {
-            switch (res)
-            {
-            case(10):
-                cout << 'A';
-                break;
-            case(11):
-                cout << 'B';
-                break;
-            case(12):
-                cout << 'C';
-                break;
-            case(13):
-                cout << 'D';
-                break;
-            case(14):
-                cout << 'E';
-                break;
-            case(15):
-                cout << 'F';
-                break;
-            case(16):
-                cout << 'G';
-                break;
-            case(17):
-                cout << 'H';
-                break;
-            case(18):
-                cout << 'I';
-                break;
-            case(19):
-                cout << 'J';
-                break;
-            case(20):
-                cout << 'K';
-                break;
-            case(21):
-                cout << 'L';
-                break;
-            case(22):
-                cout << 'M';
-                break;
-            case(23):
-                cout << 'N';
-                break;
-            case(24):
-                cout << 'O';
-                break;
-            case(25):
-                cout << 'P';
-                break;
-            case(26):
-                cout << 'Q';
-                break;
-            case(27):
-                cout << 'R';
-                break;
-            case(28):
-                cout << 'S';
-                break;
-            case(29):
-                cout << 'T';
-                break;
-            case(30):
-                cout << 'U';
-                break;
-            case(31):
-                cout << 'V';
-                break;
-            case(32):
-                cout << 'W';
-                break;
-            case(33):
-                cout << 'X';
-                break;
-            case(34):
-                cout << 'Y';
-                break;
-            case(35):
-                cout << 'Z';
-                break;
-            default:
-                cout << res;
-                break;
-            }
+            Out(res);
         }
         else
         {
-            switch (num)
-            {
-            case(10):
-                cout << 'A';
-                break;
-            case(11):
-                cout << 'B';
-                break;
-            case(12):
-                cout << 'C';
-                break;
-            case(13):
-                cout << 'D';
-                break;
-            case(14):
-                cout << 'E';
-                break;
-            case(15):
-                cout << 'F';
-                break;
-            case(16):
-                cout << 'G';
-                break;
-            case(17):
-                cout << 'H';
-                break;
-            case(18):
-                cout << 'I';
-                break;
-            case(19):
-                cout << 'J';
-                break;
-            case(20):
-                cout << 'K';
-                break;
-            case(21):
-                cout << 'L';
-                break;
-            case(22):
-                cout << 'M';
-                break;
-            case(23):
-                cout << 'N';
-                break;
-            case(24):
-                cout << 'O';
-                break;
-            case(25):
-                cout << 'P';
-                break;
-            case(26):
-                cout << 'Q';
-                break;
-            case(27):
-                cout << 'R';
-                break;
-            case(28):
-                cout << 'S';
-                break;
-            case(29):
-                cout << 'T';
-                break;
-            case(30):
-                cout << 'U';
-                break;
-            case(31):
-                cout << 'V';
-                break;
-            case(32):
-                cout << 'W';
-                break;
-            case(33):
-                cout << 'X';
-                break;
-            case(34):
-                cout << 'Y';
-                break;
-            case(35):
-                cout << 'Z';
-                break;
-            default:
-                cout << num;
-                break;
-            }
-            switch (res)
-            {
-            case(10):
-                cout << 'A';
-                break;
-            case(11):
-                cout << 'B';
-                break;
-            case(12):
-                cout << 'C';
-                break;
-            case(13):
-                cout << 'D';
-                break;
-            case(14):
-                cout << 'E';
-                break;
-            case(15):
-                cout << 'F';
-                break;
-            case(16):
-                cout << 'G';
-                break;
-            case(17):
-                cout << 'H';
-                break;
-            case(18):
-                cout << 'I';
-                break;
-            case(19):
-                cout << 'J';
-                break;
-            case(20):
-                cout << 'K';
-                break;
-            case(21):
-                cout << 'L';
-                break;
-            case(22):
-                cout << 'M';
-                break;
-            case(23):
-                cout << 'N';
-                break;
-            case(24):
-                cout << 'O';
-                break;
-            case(25):
-                cout << 'P';
-                break;
-            case(26):
-                cout << 'Q';
-                break;
-            case(27):
-                cout << 'R';
-                break;
-            case(28):
-                cout << 'S';
-                break;
-            case(29):
-                cout << 'T';
-                break;
-            case(30):
-                cout << 'U';
-                break;
-            case(31):
-                cout << 'V';
-                break;
-            case(32):
-                cout << 'W';
-                break;
-            case(33):
-                cout << 'X';
-                break;
-            case(34):
-                cout << 'Y';
-                break;
-            case(35):
-                cout << 'Z';
-                break;
-            default:
-                cout << res;
-                break;
-            }
+            Out(num);
+            Out(res);
         }
     }
 }
 int main()
 {
     setlocale(LC_ALL, "ru");
-    int numS=-1,n;
+    int numS=-1;
     long int num;
-    long double res;
     cout << "Введите натуральное число"<<endl;
     cin >> num;
     while (numS<=0||numS>35)
