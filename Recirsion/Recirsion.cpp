4bc98103e2403a7208897a4de22aa2ce3655ba3c
TimCheers <73362594+TimCheers@users.noreply.github.com>
2020-12-10 22:10:07 +0500

    Добавьте файлы проекта.

---
+++ Recirsion/Recirsion.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void foo(int num, int numS)
+{
+    int res;
+    res = num % numS;
+    num /= numS;
+    /*res = num % numS;*/
+    if (num>=numS)
+    {
+        foo(num, numS);
+        cout << res;
+    }
+    else
+    {
+        cout << num;
+        cout << res;
+        
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    int num, numS;
+    float res;
+    cout << "Введите натуральное число"<<endl;
+    cin >> num;
+    cout << "Введите количество знаков новой системы счисления"<<endl;
+    cin >> numS;
+    foo(num, numS);
+    
+    
+}
+
+
